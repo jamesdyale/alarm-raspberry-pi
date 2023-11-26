7458f05603781334c6a08f2ed45d7f5af76e56f8
James Odeyale <jamesodeyale01@gmail.com>
2023-11-26 12:09:17 -0500

    testing

--- sensor.py
+++ sensor.py
@@ -68,24 +68,16 @@ def check_motion_and_update_data(alarm):
     update_trigger_alarm(alarm["id"], True, current_utc_time)
     return
 
-def MOTION(PIR_PIN):
-    print("Motion Detected!")
-
-print("PIR Module Test (CTRL+C to exit)")
-sleep(2)
-print("Ready")
-
 
 try:
-    # GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=MOTION)
     while True:
-        # print("App running")
-        # alarms = db.child("alarms").get()
-        #isTimeMatched, alarm  = matchTime(alarms.val())
+        print("App running")
+        alarms = db.child("alarms").get()
+        isTimeMatched, alarm  = match_time(alarms.val())
         if GPIO.input(PIR_PIN):
                print("Motion Detected")
+               check_motion_and_update_data(alarm)
         sleep(1)
 except KeyboardInterrupt:
     print("Exiting...")
-
-GPIO.cleanup()
+    GPIO.cleanup()
